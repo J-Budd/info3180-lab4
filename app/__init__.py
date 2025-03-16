7fb17c32d448c31cc77197c9d1bba77d9c1044f4
Jillian Budd <jillianabudd@gmail.com>
2025-03-15 20:32:47 -0500

    Setup database connection and migration starter

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from .config import Config
 # import flask migrate here
 
@@ -14,5 +15,7 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+
+migrate = Migrate(app, db)
 
 from app import views
